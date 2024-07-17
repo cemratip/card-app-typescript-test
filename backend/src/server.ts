384dc91267c123ddc6eb3f64e2447eae4a490ba3
cemratip <cemratip@outlook.com>
2024-07-17 23:50:08 +0100

    Bug fix

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -30,6 +30,9 @@ server.post<{ Body: Entry }>("/create/", async (req, reply) => {
   newEntryBody.created_at
     ? (newEntryBody.created_at = new Date(req.body.created_at))
     : (newEntryBody.created_at = new Date());
+  newEntryBody.scheduled_at
+    ? (newEntryBody.scheduled_at = new Date(req.body.scheduled_at))
+    : (newEntryBody.scheduled_at = new Date());
   try {
     const createdEntryData = await Prisma.entry.create({ data: req.body });
     reply.send(createdEntryData);
